055a8ba764a38e26849852b95c37bc12eadcd395
Jorge Salcedo <165866373+jorgelso@users.noreply.github.com>
2025-02-17 12:12:24 -0600

    Create page.tsx

---
+++ 17feb-app/src/app/page3/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../Components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col justify-center items-center text-center w-full h-full min-h-screen pt-16">
+        <p className="text-4xl">En construccion</p>
+      </div>
+    </>
+  );
+}
